76ff713208e115f365bfd61d0ec5f57bb723391b
firstova-a <firstova-a@yandex.ru>
2021-04-29 17:25:30 +0300

    Добавьте файлы проекта.

---
+++ lab6/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+
+        private const int serverPort = 2365;
+        private static TcpClient client;
+        private static string message;
+        
+        private static bool play = true;
+
+        static async Task Main(string[] args)
+        {
+            client = new TcpClient();
+            await client.ConnectAsync(IPAddress.Loopback, serverPort);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            Console.WriteLine("User start");
+            
+            while (play)
+            {
+
+                bool run = true;
+                var stones = 0;
+                var max_step = 0;
+                var stones_s = "";
+                var max_step_s = "";
+                Console.Write("Enter the number of stones: ");
+                stones_s = Console.ReadLine();
+                Console.Write("Enter max step: ");
+                max_step_s = Console.ReadLine();
+                do
+                {
+                    if (int.TryParse(stones_s, out stones) && int.TryParse(max_step_s, out max_step) && (max_step > 0) && (stones > 0))
+                    {
+                        message = stones_s + ' ' + max_step_s;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input correct numbers");
+                    }
+
+                } while ((stones == 0) && (max_step == 0));
+
+                
+                await writer.WriteLineAsync(message);
+                await writer.FlushAsync();
+                string answer = await reader.ReadLineAsync();
+                Console.WriteLine(answer);
+                while (run)
+                {
+                    var step_s = "";
+                    var step = 0;
+                    do
+                    {
+                        Console.Write("Enter your step: ");
+                        step_s = Console.ReadLine();
+                        if (int.TryParse(step_s, out step) && (step > 0) && (step <= max_step))
+                        {
+                            Console.WriteLine("OK");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Uncorrect step");
+                        }
+                    } while (!(int.TryParse(step_s, out step) && (step > 0) && (step <= max_step)));
+                    await writer.WriteLineAsync(step_s);
+                    await writer.FlushAsync();
+                    answer = await reader.ReadLineAsync();
+                    if ((answer == "I am winner, ha-ha!") || (answer == "You are winner!"))
+                    {
+                        run = false;
+                    }
+                    Console.WriteLine(answer);
+                }
+                Console.WriteLine("Want to play again? any character or no");
+                if (Console.ReadLine() == "no")
+                {
+                    play = false;
+                }
+
+
+            }
+            client.Close();
+        }
+    }
+}
---
+++ lab6server/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Server
+{
+    class Program
+    {
+        private static TcpListener listener;
+        private const int serverPort = 2365;
+        private static bool run;
+        private static int max_iter;
+        private static int stones;
+
+        private static TcpClient client;
+        private static NetworkStream stream;
+        private static StreamReader reader;
+        private static StreamWriter writer;
+
+
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Server start");
+            listener = new TcpListener(IPAddress.Any, serverPort);
+            run = true;
+            while (true)
+            {
+                await Listen();
+            }
+            }
+
+        private static async Task Listen()
+        {
+            listener.Start();
+            client = await listener.AcceptTcpClientAsync();
+            stream = client.GetStream();
+            reader = new StreamReader(stream);
+            writer = new StreamWriter(stream);
+
+
+            while (run)
+            {
+
+
+                bool game_data = false;
+                while (game_data == false)
+                {
+                    string message = await reader.ReadLineAsync();
+                    if (message != null)
+                    {
+                        string[] listMessage = message.Split(" ");
+                        if (int.TryParse(listMessage[0], out stones) && int.TryParse(listMessage[1], out max_iter) && (stones > 0) && (max_iter > 0))
+                        {
+                            game_data = true;
+                        }
+                    }
+                }
+               
+                    await Game();
+                
+                
+
+
+            }
+
+
+
+        }
+
+        private static async Task Game()
+        {
+            int client_step;
+            int serv_step;
+            string to_client;
+            Random random = new Random();
+            bool game = true;
+            while (game)
+            {
+                if (stones > 0)
+                {
+                    if (stones % (max_iter + 1) == 0)
+                    {
+                        serv_step = random.Next(1, max_iter);
+                    }
+                    else
+                    {
+                        serv_step = stones % (max_iter + 1);
+                    }
+
+                    stones -= serv_step;
+                    if (stones < 1)
+                    {
+                        writer = new StreamWriter(stream);
+                        await writer.WriteLineAsync("I am winner, ha-ha!");
+                        await writer.FlushAsync();
+                        game = false;
+                        break;
+                    }
+                    to_client = "There were " + (stones + serv_step).ToString() + " stones. The server removed " + serv_step.ToString() + " .Remaining: " + stones.ToString() + ".";
+
+                    await writer.WriteLineAsync(to_client);
+                    await writer.FlushAsync();
+                    if (int.TryParse(await reader.ReadLineAsync(), out client_step))
+                    {
+                        stones -= client_step;
+                        if (stones < 1)
+                        {
+                            
+                            await writer.WriteLineAsync("You are winner!");
+                            await writer.FlushAsync();
+                            game = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
